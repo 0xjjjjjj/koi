1b3dcd5583c609e6e3d721b25f3d5a6fc013583d
0xjjjjjj <0xjjjjjj@users.noreply.github.com>
2026-02-18 17:39:35 -0600

    add app icon

--- bundle/build-app.sh
+++ bundle/build-app.sh
@@ -7,5 +7,5 @@ mkdir -p "$APP/Contents/MacOS"
 mkdir -p "$APP/Contents/Resources"
 cp target/release/koi "$APP/Contents/MacOS/koi"
 cp bundle/Info.plist "$APP/Contents/Info.plist"
-# cp bundle/koi.icns "$APP/Contents/Resources/koi.icns"  # when icon exists
+cp bundle/koi.icns "$APP/Contents/Resources/koi.icns"
 echo "Built $APP"
